87dcc89a16dc899217c700d32aeb76be1cd35f87
Søren Dyrby <s@dyr.by>
2019-03-15 06:37:04 +0100

    Add files via upload

---
+++ poa/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace poa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.readerBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 65);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // readerBox
+            // 
+            this.readerBox.Location = new System.Drawing.Point(147, 12);
+            this.readerBox.Multiline = true;
+            this.readerBox.Name = "readerBox";
+            this.readerBox.Size = new System.Drawing.Size(172, 358);
+            this.readerBox.TabIndex = 2;
+            this.readerBox.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 382);
+            this.Controls.Add(this.readerBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Project office automation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox readerBox;
+    }
+}
+
---
+++ poa/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using Exl = Microsoft.Office.Interop.Excel;
+
+namespace poa
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            Exl.Application xlApp;
+            Exl.Workbook xlWorkBook;
+            Exl.Worksheet xlWorkSheet;
+            Exl.Range range;
+
+            string str;
+            int rowCount;
+            int columnCount;
+            int row = 0;
+            int column = 0;
+
+            xlApp = new Exl.Application();
+            //xlWorkBook = xlApp.Workbooks.Open(@"d:\csharp-Excel.xls", 0, true, 5, "", "", true, Exl.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkBook = xlApp.Workbooks.Open(@"C:\Udvikling\po\data\tst1.xlsx");
+            xlWorkSheet = (Exl.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            row = range.Rows.Count;
+            column = range.Columns.Count;
+
+            //for (rowCount = 1; rowCount <= row; rowCount++)
+            //{
+            //    for (columnCount = 1; columnCount <= column; columnCount++)
+            //    {
+            //        str = (string)(range.Cells[rowCount, columnCount] as Exl.Range).Value2;
+            //        MessageBox.Show(str);
+            //    }
+            //}
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= column; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        Console.Write(range.Cells[i, j].Value2.ToString() + "\t");
+
+                    //add useful things here!   
+                    if (j == 1)
+                        readerBox.Text += "\r\n";
+
+                    //write the value to the textbox
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        readerBox.Text += range.Cells[i, j].Value2.ToString() + "\t";
+
+
+                    //progress
+                    // Wait 100 milliseconds.
+                    Thread.Sleep(100);
+                    // Report progress.
+                }
+            }
+
+            Console.Write("\r\n");
+            Console.WriteLine("DONE READING FILE");
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+
+            //string name = "Items";
+            //string constr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" +
+            //                    "C:\\Sample.xlsx" +
+            //                    ";Extended Properties='Excel 12.0 XML;HDR=YES;';";
+
+            //OleDbConnection con = new OleDbConnection(constr);
+            //OleDbCommand oconn = new OleDbCommand("Select * From [" + name + "$]", con);
+            //con.Open();
+
+            //OleDbDataAdapter sda = new OleDbDataAdapter(oconn);
+            //DataTable data = new DataTable();
+            //sda.Fill(data);
+            //dataGridView1.DataSource = data;
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+
+    }
+}
